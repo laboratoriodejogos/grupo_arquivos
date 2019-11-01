1ffc5983b977894de29204b100be8e2bd1c051ad
Daniel <daniel.fb.jr@gmail.com>
2019-11-01 06:12:07 -0300

    PlayMenu

---
+++ Assets/MenuPlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPlay : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
